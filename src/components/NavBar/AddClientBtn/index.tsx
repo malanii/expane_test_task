8b4da2842a72da4343da477bb8bc5cb7946445b0
Yana Malanii <miramis134@gmail.com>
2021-03-01 15:45:52 +0200

    changed navbar btn color / example of commit in new branch

--- src/components/NavBar/AddClientBtn/index.tsx
+++ src/components/NavBar/AddClientBtn/index.tsx
@@ -3,7 +3,7 @@ import { BtnsText } from "../../../constants/ComponentsData";
 
 export const AddClientBtn: React.FC = () => {
   return (
-    <button className="bg-white px-3 rounded-full py-3 px-6 text-green-600 focus:outline-none font-semibold">
+    <button className="bg-white px-3 rounded-full py-3 px-6 text-green-500 focus:outline-none font-semibold">
       {BtnsText.addBtn}
     </button>
   );
